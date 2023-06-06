d6b9188634a612b47df84f7551d506df0dca5744
unknown <lojainhteabat@gmail.com>
2023-06-06 15:09:28 -0700

    First commit

---
+++ AjaxExampleController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class AjaxExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public JsonResult GetNames()
+        {
+            var names = new string[3]
+            {
+                "Lojain","Evan","Shahed"
+            };
+            return new JsonResult(names);
+        }
+
+
+        [HttpPost]
+        public JsonResult PostName(string name)
+        {
+           
+
+            return new JsonResult(Ok());
+
+        }
+
+
+
+    }
+}
